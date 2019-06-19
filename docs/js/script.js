3f9837c549f3cbe35b8608803ccdd48bc0a15602
AnastasiaDrokina <46569895+AnastasiaDrokina@users.noreply.github.com>
2019-06-19 18:14:36 +0200

    Add docs folder

---
+++ docs/js/script.js
@@ -0,0 +1,24 @@
+var navMain = document.querySelector('.nav');
+var navToggle = document.querySelector('.nav__toggle');
+var map = document.querySelector('.map__js');
+
+navMain.classList.remove('nav--nojs');
+navToggle.classList.remove('nav__toggle--opened');
+
+navToggle.addEventListener('click', function() {
+  if (navToggle.classList.contains('nav__toggle--opened')) {
+    navToggle.classList.remove('nav__toggle--opened');
+  } else {
+    navToggle.classList.add('nav__toggle--opened');
+  }
+
+  if (navMain.classList.contains('nav--closed')) {
+    navMain.classList.remove('nav--closed');
+    navMain.classList.add('nav--opened');
+  } else {
+    navMain.classList.add('nav--closed');
+    navMain.classList.remove('nav--opened');
+  }
+});
+
+map.style.display = 'block';
